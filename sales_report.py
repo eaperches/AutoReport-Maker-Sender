66a5e1a1c0db7711ff7540b28df6b26b3fdd6ae6
eaperches <44380252+eaperches@users.noreply.github.com>
2020-03-01 12:51:06 -0500

    Language change

--- sales_report.py
+++ sales_report.py
@@ -47,21 +47,21 @@ def create_csv(name, data):
         thewriter=csv.writer(f)
         
         #Create header
-        thewriter.writerow(['AÑO', 'MES', 'DIA', 'VTA_DET_SUBTOTAL'])
+        thewriter.writerow(['YEAR', 'MONTH', 'DAY', 'SALE'])
         #Mes num a nombre
         meses = {
-                 1 : 'Enero',
-                 2 : 'Febrero',
-                 3 : 'Marzo',
-                 4 : 'Abril',
-                 5 : 'Mayo',
-                 6 : 'Junio',
-                 7 : 'Julio',
-                 8 : 'Agosto',
-                 9 : 'Septiembre',
-                 10 : 'Octubre',
-                 11 : 'Noviembre',
-                 12 : 'Diciembre'
+                 1 : 'January',
+                 2 : 'February',
+                 3 : 'March',
+                 4 : 'April',
+                 5 : 'May',
+                 6 : 'June',
+                 7 : 'July',
+                 8 : 'August',
+                 9 : 'September',
+                 10 : 'October',
+                 11 : 'November',
+                 12 : 'December'
         }
         
         #Crear filas
